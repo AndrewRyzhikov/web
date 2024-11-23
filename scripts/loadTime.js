2ab43722b6b727312fe0ce0da6bbec4070c7cdc3
aryzhikov <aryzhikov@ozon.ru>
2024-11-23 10:01:18 +0300

    [lab-5] removed loadTime

--- scripts/loadTime.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('load', () => {
-    const loadTime = (performance.now() / 1000).toFixed(3);
-    document.getElementById('loadTime').textContent = `Page load time is ${loadTime} seconds`;
-});
